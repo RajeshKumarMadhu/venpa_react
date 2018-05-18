db9eff0270d3cc5278623f6da7b93df0b334fa85
RajeshMadhu <Seyalfactory@gmail.com>
2018-05-18 17:19:20 -0400

    Styling for container.

--- src/component/kural.js
+++ src/component/kural.js
@@ -4,10 +4,10 @@ const Kural = ({kural,id}) =>{
   var kuralObj = kural;
   return (
       <li className="list-group-item box-emphasize">
-        <a data-toggle="collapse" data-target={'#O'+id} aria-expanded="false" aria-controls="{'O'+id}">
+        <div className="kural-container">
         <p>{kuralObj.kural[0]}</p>
         <p>{kuralObj.kural[1]}</p>
-        </a>
+        </div>
         <div id={'O'+id}>{kuralObj.meaning.ta_mu_va}</div>
       </li>
   )
